f0a31a2c8bb5d8a8076873476e717b6898de11a2
Tun186 <Tun186@users.noreply.github.com>
2026-06-07 22:53:50 +0630

    Enable error reporting to diagnose live server 500 errors

--- public/index.php
+++ public/index.php
@@ -1,4 +1,9 @@
 <?php
+// Force error reporting on to diagnose the 500 Internal Server Error
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once '../app/init.php';
 
 $app = new App();
